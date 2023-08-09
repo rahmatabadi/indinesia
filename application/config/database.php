5b4a9fd3a93548a1c8df6a0506d5ca63d11343ae
Suharjo <suharjora@gmail.com>
2023-08-09 23:18:31 +0700

    push

--- application/config/database.php
+++ application/config/database.php
@@ -73,34 +73,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-$db['default'] = array(
-	'dsn' => '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'helpdesk',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
-
 // $db['default'] = array(
 // 	'dsn' => '',
 // 	'hostname' => 'localhost',
-// 	'username' => 'suha9298_indinesia',
-// 	'password' => '1ND1nesi@',
-// 	'database' => 'suha9298_indi_helpdesk',
+// 	'username' => 'root',
+// 	'password' => '',
+// 	'database' => 'helpdesk',
 // 	'dbdriver' => 'mysqli',
 // 	'dbprefix' => '',
 // 	'pconnect' => FALSE,
@@ -116,3 +94,25 @@ $db['default'] = array(
 // 	'failover' => array(),
 // 	'save_queries' => TRUE
 // );
+
+$db['default'] = array(
+	'dsn' => '',
+	'hostname' => 'localhost',
+	'username' => 'suha9298_indinesia',
+	'password' => '1ND1nesi@',
+	'database' => 'suha9298_indi_helpdesk',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
